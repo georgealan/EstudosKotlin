89a64a56cedbd7045192cac7f72bb508ef97f4e1
George Alan <georgealanrufo@gmail.com>
2020-03-30 18:46:13 -0300

    Adicionado library Dokka para documentação

---
+++ KotlinApprentice/src/main/kotlin/com/kotlin/apprentice/types/TypeConversion.kt
@@ -0,0 +1,16 @@
+package com.kotlin.apprentice.types
+
+/**
+ * @author George Alan Rufo
+ * @return as classes todas poxa
+ */
+fun main() {
+
+    /**
+     * @param id é um tipo membro do código
+     *
+     */
+    fun exampleDocumentation(id: Long, textField: String): String {
+        return "Foi certo"
+    }
+}
